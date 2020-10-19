1d297a2e1d634a8712b14a4fd4caf3bd977ec168
chfw <wangc_2011@hotmail.com>
2020-10-19 23:12:01 +0100

    :green_heart: add coverage

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-pytest tests --cov=robotchef_cli --doctest-glob=*.rst && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
+pytest tests --cov=robotchef_v2 --cov=robotchef_britishcuisine --cov=robotchef_api --doctest-glob=*.rst && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
